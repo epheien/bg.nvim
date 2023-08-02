79ffe146aad5f6d2a1cb14e43d2f2797c9fec6d2
eph <eph@MacPro.local>
2023-08-02 14:37:19 +0800

    优化加载过程, 在iTerm终端默认不加载

--- plugin/bg.lua
+++ plugin/bg.lua
@@ -1,29 +1,35 @@
-local handle = io.popen("tty")
-local tty = handle:read("*a")
-handle:close()
-
 local reset = function()
-	os.execute('printf "\\033]111\\007" > ' .. tty)
+  os.execute('printf "\\033]111\\007" > ' .. tty)
 end
 
 local update = function()
-	local normal = vim.api.nvim_get_hl_by_name("Normal", true)
-	local bg = normal["background"]
-	local fg = normal["foreground"]
-	if bg == nil then
-		return reset()
-	end
+  local normal = vim.api.nvim_get_hl_by_name("Normal", true)
+  local bg = normal["background"]
+  local fg = normal["foreground"]
+  if bg == nil then
+    return reset()
+  end
 
-	local bghex = string.format("#%06x", bg)
-	os.execute('printf "\\033]11;' .. bghex .. '\\007" > ' .. tty)
+  local bghex = string.format("#%06x", bg)
+  os.execute('printf "\\033]11;' .. bghex .. '\\007" > ' .. tty)
 
-	local fghex = string.format("#%06x", fg)
-	os.execute('printf "\\033]12;' .. fghex .. '\\007" > ' .. tty)
+  local fghex = string.format("#%06x", fg)
+  os.execute('printf "\\033]12;' .. fghex .. '\\007" > ' .. tty)
 end
 
 local setup = function()
-	vim.api.nvim_create_autocmd({ "ColorScheme", "UIEnter" }, { callback = update })
-	vim.api.nvim_create_autocmd({ "VimLeavePre" }, { callback = reset })
+  -- 已经处理了背景色问题的终端就不需要运行了
+  if vim.fn.match(vim.fn.getenv('TERM_PROGRAM'), "iTerm") == 0 then
+    return
+  end
+
+  -- 这个IO应该比较快?
+  local handle = io.popen("tty")
+  tty = handle:read("*a") -- global var
+  handle:close()
+
+  vim.api.nvim_create_autocmd({ "ColorScheme", "UIEnter" }, { callback = update })
+  vim.api.nvim_create_autocmd({ "VimLeavePre" }, { callback = reset })
 end
 
 setup()
